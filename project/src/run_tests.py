67414f54729807ebb27f4b9abf4530036864bdd6
Ibrahim Awwal <ibrahim.awwal@gmail.com>
2016-02-25 17:26:58 -0800

    Write output to results.json

--- project/src/run_tests.py
+++ project/src/run_tests.py
@@ -3,4 +3,5 @@ from json_test_runner import JSONTestRunner
 
 if __name__ == '__main__':
     suite = unittest.defaultTestLoader.discover('tests')
-    JSONTestRunner(verbosity=2, buffer=True).run(suite)
+    with open('results.json', 'w') as f:
+        JSONTestRunner(stream=f, verbosity=2, buffer=True).run(suite)
