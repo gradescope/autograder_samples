00d9767f5b991287a72a6c6013316ea40fedb602
Ibrahim Awwal <ibrahim.awwal@gmail.com>
2016-03-01 17:20:19 -0800

    More tests

---
+++ project/src/tests/test_integration.py
@@ -0,0 +1,39 @@
+import unittest
+from autograder_utils.decorators import weight, tags
+import subprocess32 as subprocess
+
+
+class TestIntegration(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    @weight(1)
+    @tags("integration")
+    def test_single_input(self):
+        """Tests the full REPL"""
+        calc = subprocess.Popen('python -u calculator.py'.split(),
+                                stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+        prompt = calc.stdout.read(2)  # Need to get the prompt off of stdout
+        calc.stdin.write("1 + 1\n")
+        calc.stdin.flush()
+
+        output = calc.stdout.readline()
+        self.assertEqual(output, "2\n")
+        calc.terminate()
+
+    @weight(1)
+    @tags("integration")
+    def test_quit(self):
+        """Test quitting the REPL"""
+        calc = subprocess.Popen('python -u calculator.py'.split(),
+                                stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+        calc.stdin.write("quit\n")
+        calc.stdin.flush()
+
+        try:
+            returncode = calc.wait(1)
+        except:
+            pass
+        if returncode is None:
+            calc.terminate()
+        self.assertIsNotNone(returncode)
---
+++ project/src/tests/test_lexer.py
@@ -0,0 +1,29 @@
+import unittest
+from autograder_utils.decorators import weight, tags
+from framework import Calculator
+
+
+class TestLexer(unittest.TestCase):
+    def setUp(self):
+        self.calc = Calculator()
+
+    @weight(1)
+    @tags("lexing")
+    def test_lex1(self):
+        """Test lexing a simple expression"""
+        tokens = self.calc.lexer("1 + 1")
+        self.assertEqual(tokens, [1, "+", 1])
+
+    @weight(1)
+    @tags("lexing")
+    def test_lex2(self):
+        """Test lexing a simple expression without whitespace"""
+        tokens = self.calc.lexer("1+1")
+        self.assertEqual(tokens, [1, "+", 1])
+
+    @weight(2)
+    @tags("lexing")
+    def test_lex3(self):
+        """Test lexing an expression with parentheses"""
+        tokens = self.calc.lexer("2*(1+1)")
+        self.assertEqual(tokens, [2, "*", "(", 1, "+", 1, ")"])
