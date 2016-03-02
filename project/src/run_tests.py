29a2e13208ba1e209ffeea678c4de5916dcf419b
Ibrahim Awwal <ibrahim.awwal@gmail.com>
2016-03-01 18:31:27 -0800

    Fix run_tests.py

--- project/src/run_tests.py
+++ project/src/run_tests.py
@@ -4,4 +4,4 @@ from gradescope_utils.autograder_utils.json_test_runner import JSONTestRunner
 if __name__ == '__main__':
     suite = unittest.defaultTestLoader.discover('tests')
     with open('results.json', 'w') as f:
-        JSONTestRunner(verbosity=2, buffer=True).run(suite)
+        JSONTestRunner(verbosity=2, stream=f, buffer=True).run(suite)
