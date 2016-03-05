1f4e600a782be44ef3221a4ec37dea78c27a06c2
Ibrahim Awwal <ibrahim.awwal@gmail.com>
2016-03-05 02:30:14 -0600

    run_tests.py writes to stdout
    run_autograder redirects it to /autograder/results/results.json.

--- project/src/run_tests.py
+++ project/src/run_tests.py
@@ -3,5 +3,4 @@ from gradescope_utils.autograder_utils.json_test_runner import JSONTestRunner
 
 if __name__ == '__main__':
     suite = unittest.defaultTestLoader.discover('tests')
-    with open('/autograder/results/results.json', 'w') as f:
-        JSONTestRunner(stream=f).run(suite)
+    JSONTestRunner().run(suite)
