1afe831a4162c399358ecfb0a612b3cf633b8fb4
Ibrahim Awwal <ibrahim.awwal@gmail.com>
2016-03-05 00:31:29 -0600

    Changed defaults of JSONTestRunner so that this is less verbose
    The verbosity parameter doesn't actually do anything yet anyway.

--- project/src/run_tests.py
+++ project/src/run_tests.py
@@ -4,4 +4,4 @@ from gradescope_utils.autograder_utils.json_test_runner import JSONTestRunner
 if __name__ == '__main__':
     suite = unittest.defaultTestLoader.discover('tests')
     with open('/autograder/results/results.json', 'w') as f:
-        JSONTestRunner(verbosity=2, stream=f, buffer=True).run(suite)
+        JSONTestRunner(stream=f).run(suite)
