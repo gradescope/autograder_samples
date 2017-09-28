236a658f17cefc7d58272f763e2f44bd96be54e6
Ibrahim Awwal <ibrahim.awwal@gmail.com>
2017-09-28 05:30:36 -0700

    Add an example for using the leaderboard decorator
    This one just spits out random scores.

---
+++ python/src/tests/test_leaderboard.py
@@ -0,0 +1,23 @@
+import unittest
+from random import randint
+from gradescope_utils.autograder_utils.decorators import leaderboard
+
+
+class TestLeaderboard(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    @leaderboard("high_score")
+    def test_leaderboard(self, set_leaderboard_value=None):
+        """Sets a leaderboard value"""
+        set_leaderboard_value(randint(0, 10))
+
+    @leaderboard("stars")
+    def test_string(self, set_leaderboard_value=None):
+        """Sets a leaderboard value"""
+        set_leaderboard_value("*" * randint(0, 10))
+
+    @leaderboard("another")
+    def test_another(self, set_leaderboard_value=None):
+        """Sets a leaderboard value"""
+        set_leaderboard_value(randint(0, 10))
