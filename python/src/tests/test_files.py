a09a6a3ceb2f78b0ca4909c004ecac3792ef90c0
Ibrahim Awwal <ibrahim.awwal@gmail.com>
2018-01-29 00:17:38 -0800

    Add a unit test for required files

---
+++ python/src/tests/test_files.py
@@ -0,0 +1,14 @@
+import unittest
+from gradescope_utils.autograder_utils.decorators import weight
+from gradescope_utils.autograder_utils.files import check_submitted_files
+
+
+class TestFiles(unittest.TestCase):
+    @weight(0)
+    def test_submitted_files(self):
+        """Check submitted files"""
+        missing_files = check_submitted_files(['calculator.py'])
+        for path in missing_files:
+            print 'Missing {0}'.format(path)
+        self.assertEqual(len(missing_files), 0, 'Missing some required files!')
+        print 'All required files submitted!'
