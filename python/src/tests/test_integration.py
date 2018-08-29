9212ed995afc485d09bb78c41255794f4d1b07e0
Ibrahim Awwal <ibrahim.awwal@gmail.com>
2018-08-28 17:07:04 -0700

    Use Popen.communicate instead of writing/reading stdin/stdout directly
    This makes the tests a little less brittle, and also has a built in
    timeout feature.

--- python/src/tests/test_integration.py
+++ python/src/tests/test_integration.py
@@ -13,12 +13,10 @@ class TestIntegration(unittest.TestCase):
         """Evaluate 1 + 1 in the REPL"""
         calc = subprocess.Popen('python -u calculator.py'.split(),
                                 stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-        prompt = calc.stdout.read(2)  # Need to get the prompt off of stdout
-        calc.stdin.write("1 + 1\n")
-        calc.stdin.flush()
-
-        output = calc.stdout.readline()
-        self.assertEqual(output, "2\n")
+        output, err = calc.communicate("1 + 1\n", 1)
+        self.assertTrue(output.startswith(">"))    # Check for presence of prompt
+        answer = output[1:].split()[0]             # Separate prompt from answer
+        self.assertEqual(answer.strip(), "2")
         calc.terminate()
 
     @weight(2)
@@ -27,13 +25,9 @@ class TestIntegration(unittest.TestCase):
         """Quit the REPL"""
         calc = subprocess.Popen('python -u calculator.py'.split(),
                                 stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-        calc.stdin.write("quit\n")
-        calc.stdin.flush()
+        calc.communicate("quit\n", 1)
 
-        try:
-            returncode = calc.wait(1)
-        except:
-            pass
+        returncode = calc.returncode
         if returncode is None:
             calc.terminate()
         self.assertIsNotNone(returncode)
