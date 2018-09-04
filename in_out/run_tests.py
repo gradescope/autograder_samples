cf51f915c5fe74010b57af2362aa7fca323663b8
Ibrahim Awwal <ibrahim.awwal@gmail.com>
2018-09-04 00:40:24 -0700

    Set up running tests and comparing output

--- in_out/run_tests.py
+++ in_out/run_tests.py
@@ -1,6 +1,11 @@
 import unittest
 import os
+import subprocess32 as subprocess
+from subprocess32 import PIPE
 from gradescope_utils.autograder_utils.json_test_runner import JSONTestRunner
+from gradescope_utils.autograder_utils.decorators import weight
+
+BASE_DIR = './test_data'
 
 
 class TestMetaclass(type):
@@ -10,16 +15,37 @@ class TestMetaclass(type):
     def __new__(cls, name, bases, attrs):
         data_dir = attrs['data_dir']
         attrs[cls.test_name(data_dir)] = cls.generate_test(data_dir)
-
         return super(TestMetaclass, cls).__new__(cls, name, bases, attrs)
 
     @classmethod
     def generate_test(cls, dir_name):
         # Return a testcase for the given directory
+        command = cls.generate_command(cls, dir_name)
+        n = 1                   # TODO: Allow configuring weight
+
+        @weight(n)
         def fn(self):
-            self.assertEqual(dir_name, None)
+            # Load command from the script
+            proc = subprocess.Popen(command, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+            with open(os.path.join(BASE_DIR, dir_name, 'input')) as f:
+                stdin = f.read()
+
+            output, err = proc.communicate(stdin, 1)  # TODO: Allow configuring timeout
+
+            # Load expected output
+            with open(os.path.join(BASE_DIR, dir_name, 'output')) as f:
+                expected_output = f.read()
+            # TODO: Allow configuring message
+            self.assertEqual(expected_output, output, msg="Output did not match expected")
         fn.__doc__ = 'Test {0}'.format(dir_name)
         return fn
+
+    @staticmethod
+    def generate_command(cls, dir_name):
+        """Generates the command passed to Popen"""
+        # TODO: Support args
+        # TODO: allow overriding in test case
+        return ["bash", "./run.sh"]
 
     @staticmethod
     def klass_name(dir_name):
@@ -42,16 +68,14 @@ def build_test_class(data_dir):
 
 
 def find_data_directories():
-    base_dir = './test_data'
     return filter(
-        lambda x: os.path.isdir(os.path.join(base_dir, x)),
-        os.listdir(base_dir)
+        lambda x: os.path.isdir(os.path.join(BASE_DIR, x)),
+        os.listdir(BASE_DIR)
     )
 
 
 if __name__ == '__main__':
     suite = unittest.TestSuite()
-    print find_data_directories()
 
     for name in find_data_directories():
         klass = build_test_class(name)
