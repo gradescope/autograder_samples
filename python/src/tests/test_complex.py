a82240ede495561bc72fe9278e049b778a41a59a
Ibrahim Awwal <ibrahim@gradescope.com>
2020-01-14 12:19:17 -0800

    Use the number decorator in some tests as an example

--- python/src/tests/test_complex.py
+++ python/src/tests/test_complex.py
@@ -1,5 +1,5 @@
 import unittest
-from gradescope_utils.autograder_utils.decorators import weight, visibility
+from gradescope_utils.autograder_utils.decorators import weight, visibility, number
 from calculator import Calculator
 
 
@@ -9,6 +9,7 @@ class TestComplex(unittest.TestCase):
 
     @weight(2)
     @visibility('after_due_date')
+    @number("2.1")
     def test_eval_parens(self):
         """Evaluate (1 + 1) * 4"""
         val = self.calc.eval("(1 + 1) * 4")
@@ -16,18 +17,21 @@ class TestComplex(unittest.TestCase):
 
     @weight(2)
     @visibility('after_due_date')
+    @number("2.2")
     def test_eval_precedence(self):
         """Evaluate 1 + 1 * 8"""
         val = self.calc.eval("1 + 1 * 8")
         self.assertEqual(val, 9)
 
     @weight(2)
+    @number("2.3")
     def test_eval_mul_div(self):
         """Evaluate 8 / 4 * 2"""
         val = self.calc.eval("8 / 4 * 2")
         self.assertEqual(val, 4)
 
     @weight(2)
+    @number("2.4")
     def test_eval_negative_number(self):
         """Evaluate -2 + 6"""
         val = self.calc.eval("-2 + 6")
--- python/src/tests/test_simple.py
+++ python/src/tests/test_simple.py
@@ -1,5 +1,5 @@
 import unittest
-from gradescope_utils.autograder_utils.decorators import weight
+from gradescope_utils.autograder_utils.decorators import weight, number
 from calculator import Calculator
 
 
@@ -8,30 +8,35 @@ class TestSimpleArithmetic(unittest.TestCase):
         self.calc = Calculator()
 
     @weight(1)
+    @number("1.1")
     def test_eval_add(self):
         """Evaluate 1 + 1"""
         val = self.calc.eval("1 + 1")
         self.assertEqual(val, 2)
 
     @weight(1)
+    @number("1.2")
     def test_eval_sub(self):
         """Evaluate 2 - 1"""
         val = self.calc.eval("2 - 1")
         self.assertEqual(val, 1)
 
     @weight(1)
+    @number("1.3")
     def test_eval_mul(self):
         """Evaluate 4 * 8"""
         val = self.calc.eval("4 * 8")
         self.assertEqual(val, 32)
 
     @weight(1)
+    @number("1.4")
     def test_eval_div(self):
         """Evaluate 8/4"""
         val = self.calc.eval("8 / 4")
         self.assertEqual(val, 2)
 
     @weight(2)
+    @number("1.5")
     def test_eval_whitespace(self):
         """Evaluate 1+1 (no whitespace)"""
         val = self.calc.eval("1+1")
