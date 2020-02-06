778b8e89725c02ea8f2fae103b036295826b5a08
Victoria Zhong <SemicolonExpected@users.noreply.github.com>
2020-02-05 20:48:07 -0500

    Update setup.sh

--- c#/src/setup.sh
+++ c#/src/setup.sh
@@ -4,4 +4,4 @@ apt install apt-transport-https dirmngr
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
 echo "deb https://download.mono-project.com/repo/ubuntu vs-bionic main" | tee /etc/apt/sources.list.d/mono-official-vs.list
 apt-get update
-apt-get install -qy nunit nunit-console monodevelop
+apt-get install -qy nunit nunit-console monodevelop monodevelop-nunit
