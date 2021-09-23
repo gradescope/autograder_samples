bbd77addf358151cbb36d18391f1af67ecb46126
Stephan Kaminsky <thaumicmekanism@gmail.com>
2021-09-23 15:23:18 -0700

    Updated mysql example to python3

--- mysql/run_tests.py
+++ mysql/run_tests.py
@@ -3,4 +3,5 @@ from gradescope_utils.autograder_utils.json_test_runner import JSONTestRunner
 
 if __name__ == '__main__':
     suite = unittest.defaultTestLoader.discover('tests')
-    JSONTestRunner(visibility='visible').run(suite)
+    with open('/autograder/results/results.json', 'w') as f:
+        JSONTestRunner(visibility='visible', stream=f).run(suite)
